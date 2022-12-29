e90fac56f349030ffe11220723ee38102c1046b5
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-12-30 00:50:16 +0530

    Added Project BAse

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,21 +10,21 @@ function Footer() {
             </div>
             <div className="footerLinks">
                 <ul>
-                    <li><a href='#'>FAQ</a></li>
-                    <li><a href='#'>Help Centre</a></li>
-                    <li><a href='#'>Account</a></li>
-                    <li><a href='#'>Media Centre</a></li>
-                    <li><a href='#'>Investor Relations</a></li>
-                    <li><a href='#'>Jobs</a></li>
-                    <li><a href='#'>Ways to Watch</a></li>
-                    <li><a href='#'>Terms of Use</a></li>
-                    <li><a href='#'>Privacy</a></li>
-                    <li><a href='#'>Cookie Preferences</a></li>
-                    <li><a href='#'>Corporate Information</a></li>
-                    <li><a href='#'>Contact Us</a></li>
-                    <li><a href='#'>Speed Test</a></li>
-                    <li><a href='#'>Legal Notices</a></li>
-                    <li><a href='#'>Only on Netflix</a></li>
+                    <li><a href='/#'>FAQ</a></li>
+                    <li><a href='/#'>Help Centre</a></li>
+                    <li><a href='/#'>Account</a></li>
+                    <li><a href='/#'>Media Centre</a></li>
+                    <li><a href='/#'>Investor Relations</a></li>
+                    <li><a href='/#'>Jobs</a></li>
+                    <li><a href='/#'>Ways to Watch</a></li>
+                    <li><a href='/#'>Terms of Use</a></li>
+                    <li><a href='/#'>Privacy</a></li>
+                    <li><a href='/#'>Cookie Preferences</a></li>
+                    <li><a href='/#'>Corporate Information</a></li>
+                    <li><a href='/#'>Contact Us</a></li>
+                    <li><a href='/#'>Speed Test</a></li>
+                    <li><a href='/#'>Legal Notices</a></li>
+                    <li><a href='/#'>Only on Netflix</a></li>
                 </ul>
             </div>
             <div>
